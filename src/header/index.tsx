0796c41d964109ee4195fac94959910da1819638
Mila Korchanova <korchanovamila29@outlook.com>
2024-09-15 11:08:20 -0700

    upload

---
+++ src/header/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+import colorStyles from "../style/color.module.css";
+import alignStyles from "../style/align.module.css";
+import sizeStyles from "../style/size.module.css";
+
+type HeaderProps = {
+  children: React.ReactNode;
+};
+
+const Header: React.FC<HeaderProps> = ({ children }) => (
+  <header
+    className={[
+      colorStyles.darkTheme,
+      alignStyles.fromStartBottom,
+      sizeStyles.height100,
+      "p-0",
+    ].join(" ")}>
+    <Col
+      xs="auto"
+      className={[sizeStyles.widthLogo, sizeStyles.height100, alignStyles.absoluteCenter].join(
+        " ",
+      )}>
+    </Col>
+    <Col>{children}</Col>
+  </header>
+);
+
+export default Header;
